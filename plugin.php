1291ee10acdca8aabde5a6ec62d583894c6bcc15
Julien Desrosiers <juliend2@gmail.com>
2012-05-26 19:25:26 -0400

    initial commit

---
+++ plugin.php
@@ -0,0 +1,11 @@
+<?php
+/* 
+Plugin Name: JDesrosiers Page Redirect
+Plugin URI: 
+Description: Simple WordPress plugin to redirect pages to other pages
+Author: Julien Desrosiers
+Version: 1.0 
+Author URI: http://www.juliendesrosiers.com
+*/
+
+
